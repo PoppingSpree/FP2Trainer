0023a99a206a073e50fb5c10783a504d5d810502
Catssandra Ann <ann@poppingspree.dev>
2022-10-03 01:23:27 -0500

    Add missing Namespace scope.

--- ForceRenderRate.cs
+++ ForceRenderRate.cs
@@ -3,38 +3,43 @@ using System.Collections;
 using System.Threading;
 using UnityEngine;
 
-// This is heavily based on the script given here: https://blog.unity.com/technology/precise-framerates-in-unity
-public class ForceRenderRate : MonoBehaviour
+namespace Fp2Trainer
 {
-    public float Rate = 60.0f;
-    float currentFrameTime;
-
-    void Start()
+// This is heavily based on the script given here: https://blog.unity.com/technology/precise-framerates-in-unity
+    public class ForceRenderRate : MonoBehaviour
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 9999;
-        currentFrameTime = Time.realtimeSinceStartup;
-        StartCoroutine("WaitForNextFrame");
-    }
+        public float Rate = 60.0f;
+        float currentFrameTime;
 
-    void OnLevelWasLoaded(int level)
-    {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 9999;
-    }
-
-    IEnumerator WaitForNextFrame()
-    {
-        while (true)
+        void Start()
         {
-            yield return new WaitForEndOfFrame();
-            currentFrameTime += 1.0f / Rate;
-            var t = Time.realtimeSinceStartup;
-            var sleepTime = currentFrameTime - t - 0.01f;
-            if (sleepTime > 0)
-                Thread.Sleep((int)(sleepTime * 1000));
-            while (t < currentFrameTime)
-                t = Time.realtimeSinceStartup;
+            Fp2Trainer.Log("SixtyFPSHack has Started");
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 9999;
+            currentFrameTime = Time.realtimeSinceStartup;
+            StartCoroutine("WaitForNextFrame");
+        }
+
+        void OnLevelWasLoaded(int level)
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 9999;
+            Fp2Trainer.Log("SixtyFPSHack refreshed.");
+        }
+
+        IEnumerator WaitForNextFrame()
+        {
+            while (true)
+            {
+                yield return new WaitForEndOfFrame();
+                currentFrameTime += 1.0f / Rate;
+                var t = Time.realtimeSinceStartup;
+                var sleepTime = currentFrameTime - t - 0.01f;
+                if (sleepTime > 0)
+                    Thread.Sleep((int)(sleepTime * 1000));
+                while (t < currentFrameTime)
+                    t = Time.realtimeSinceStartup;
+            }
         }
     }
 }
